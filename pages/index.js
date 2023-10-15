60ce80c77292bb2258ace1034a11514119cdfe7d
Styrfire <g_n_rogers1991@yahoo.com>
2023-10-15 09:00:39 -0500

    added global css

--- pages/index.js
+++ pages/index.js
@@ -43,17 +43,84 @@ export default function HomePage() {
 		}
 	}
 
+	function checkFiles(files)
+	{
+		let checkingFound = false;
+		let savingFound = false;
+		let creditFound = false;
+		if (files[0] ==  undefined || files[1] ==  undefined || files[2] ==  undefined)
+		{
+			alert("You did not submit 3 files!");
+			return false;
+		}
+
+		for (let i = 0; i < 3; i++)
+		{
+			let file = files[i];
+			console.log(file);
+
+			// console.log("filename = " + file.name)
+			let fileWords = file.name.split(' ');
+			if (fileWords.length != 3)
+			{
+				console.log("fileWords.length issue!");
+				alert(file.name + " is not a valid name!");
+				return false;
+			}
+
+			switch(i)
+			{
+				case 0:
+					if(fileWords[0] != "Checking")
+					{
+						alert(file.name + " is not the Checking file!");
+						return false;
+					}
+					break;
+				case 1:
+					if(fileWords[0] != "Saving")
+					{
+						alert(file.name + " is not the Saving file!");
+						return false;
+					}
+					break;
+				case 2:
+					if(fileWords[0] != "Credit")
+					{
+						alert(file.name + " is not the Credit file!");
+						return false;
+					}
+					break;
+			}
+
+			if (fileWords[1] != "January" && fileWords[1] != "February" && fileWords[1] != "March" && fileWords[1] != "April" && fileWords[1] != "May" && fileWords[1] != "June" && fileWords[1] != "July" && fileWords[1] != "August" && fileWords[1] != "September" && fileWords[1] != "October" && fileWords[1] != "November" && fileWords[1] != "December")
+			{
+				alert(file.name + " is not a valid name!");
+				return false;
+			}
+
+			if (!/^[0-9]{4}\.csv$/.test(fileWords[2]))
+			{
+				alert(file.name + " is not a valid name!");
+				return false;
+			}
+		};
+
+		console.log("checkFiles() returning true");
+		return true;
+	}
+
 	return (
 		<>
  			<Head>
  				<title>Monthly Financial Uploader</title>
 			</Head>
 			<div id="content">
-				<label for="checkingFile">Upload Checking CSV</label>
+				<label htmlFor="checkingFile">Upload Checking CSV</label>
 				<input id="checkingFile" type="file" name="checking" accept=".csv" /><br />
-				<label for="savingFile">Upload Saving CSV</label>
+				<label htmlFor="savingFile">Upload Saving CSV</label>
 				<input id="savingFile" type="file" name="savings" accept=".csv" /><br />
-				<label for="creditFile">Upload Credit CSV</label>
+				<label htmlFor="creditFile">Upload Credit CSV</label>
 				<input id="creditFile" type="file" name="credit_card" accept=".csv" /><br /><br />
 				<button onClick={handleClick}>Submit</button>
 			</div>
